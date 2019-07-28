0c09a052c3c6791786042396b1b3d1f849832b9d
Asaf-W-PC <A@a.com>
2019-07-28 13:39:48 +0300

    Upload Seed

---
+++ UploadPresignedURClient/Interface/IShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolareshet.UploadPresignedURClient.Interface
+{
+    public interface IShellViewModel
+    {
+    }
+}
