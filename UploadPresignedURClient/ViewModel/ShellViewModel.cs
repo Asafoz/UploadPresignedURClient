01e03fd1b4a500a75702b2e36c627f4495bfbc32
Asaf-W-PC <A@a.com>
2019-08-06 16:00:57 +0300

    Update the Progress Bar

--- UploadPresignedURClient/ViewModel/ShellViewModel.cs
+++ UploadPresignedURClient/ViewModel/ShellViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AWSWrapperS3.Contracts;
 using AWSWrapperS3.Controller;
 using Bolareshet.UploadPresignedURClient.Interface;
 using CompressModuel.Contracts.View;
@@ -13,6 +14,11 @@ using Prism.Mvvm;
 namespace Bolareshet.UploadPresignedURClient.ViewModel
 {
 
+    enum ProgressOrigineType
+    {
+        Compress,
+        UploadFile
+    }
 
     [Export(typeof(ShellViewModel))]
     public class ShellViewModel : BindableBase
@@ -21,7 +27,10 @@ namespace Bolareshet.UploadPresignedURClient.ViewModel
         private readonly ICompressUserControl m_compressUserControl;
         private string m_strFileToUpload;
         private string m_s3PresignedURL;
+
         private int m_valueProgress;
+        private int m_minimumProgress;
+        private int m_maximumProgress;
         private bool m_showTotalProcess;
 
         public DelegateCommand UploadButtonCommand { get; set; }
@@ -49,11 +58,14 @@ namespace Bolareshet.UploadPresignedURClient.ViewModel
                 Console.WriteLine(ex.Message);
             }
 
-            ShowTotalProcess = true;
+            ShowTotalProcess = false;
             m_strFileToUpload = string.Empty;
 
+            MinimumProgress = 0;
+            MaximumProgress = 100;
+
             m_compressUserControl.FileReadyNotify += new EventHandler<string>(OnFileReadyNotify);
-            m_compressUserControl.FileCompressProgressNotify += new EventHandler<int>(OnFileCompressProgressNotify);
+            m_compressUserControl.FileCompressProgressNotify += new EventHandler<Tuple<long, long>>(OnFileCompressProgressNotify);
 
 
         }
@@ -64,17 +76,49 @@ namespace Bolareshet.UploadPresignedURClient.ViewModel
             UploadButtonCommand.RaiseCanExecuteChanged();
         }
 
-        private void OnFileCompressProgressNotify(object sender, int e)
+        private void OnProgressChanged(object sender, FileTransferProgress e)
+        {
+            Task fileCompressProgressTask = Task.Factory.StartNew(() =>
+            {
+                ShowTotalProcess = true;
+                ValueProgress = CalcUploadPresignedURClientProgress((int)e.Offset, e.FileSize, ProgressOrigineType.UploadFile);
+            });
+
+            fileCompressProgressTask.Wait();
+        }
+        private void OnFileCompressProgressNotify(object sender, Tuple<long, long> e)
         {
             
             Task fileCompressProgressTask = Task.Factory.StartNew(() =>
             {
-                ValueProgress = e;
-
-                OnPropertyChanged("ValueProgress");
+                ShowTotalProcess = false;
+                ValueProgress = CalcUploadPresignedURClientProgress((int)e.Item1 , e.Item2 , ProgressOrigineType.Compress);
             });
 
             fileCompressProgressTask.Wait();
+        }
+
+        private int CalcUploadPresignedURClientProgress(int currentProgress, long maxinmumProgressSize, ProgressOrigineType origineType)
+        {
+            int convertedProgress = 0;
+
+            double pro = (double)((double)currentProgress / (double)maxinmumProgressSize);
+            convertedProgress = (int)(pro * 100);
+
+            switch (origineType)
+            {
+                case ProgressOrigineType.Compress:
+                    convertedProgress =(int) (convertedProgress  * 0.3);
+                    break;
+                case ProgressOrigineType.UploadFile:
+                    convertedProgress = 30 + (int)(convertedProgress * 0.7);
+                    break;
+                default:
+                    convertedProgress = 0;
+                    break;
+            }
+
+            return convertedProgress;
         }
 
         public string S3PresignedURL
@@ -95,7 +139,25 @@ namespace Bolareshet.UploadPresignedURClient.ViewModel
                 SetProperty(ref m_valueProgress, value);
             }
         }
-        
+
+        public int MinimumProgress
+        {
+            get { return m_minimumProgress; }
+            set
+            {
+                SetProperty(ref m_minimumProgress, value);
+            }
+        }
+
+        public int MaximumProgress
+        {
+            get { return m_maximumProgress; }
+            set
+            {
+                SetProperty(ref m_maximumProgress, value);
+            }
+        }
+
         public bool ShowTotalProcess
         {
             get { return m_showTotalProcess; }
@@ -112,8 +174,27 @@ namespace Bolareshet.UploadPresignedURClient.ViewModel
 
         private void UploadButtonExecute()
         {
-            S3ClientWrapper.UploadObjectUsingPresignedURL(S3PresignedURL, m_strFileToUpload);
+            try
+            {
+                Task compressData = Task.Factory.StartNew(() =>
+                {
+                    ShowTotalProcess = true;
+
+                    Progress<FileTransferProgress> fileTransferProgress = new Progress<FileTransferProgress>();
+                    fileTransferProgress.ProgressChanged += new EventHandler<FileTransferProgress>(OnProgressChanged);
+                    S3ClientWrapper.UploadObjectUsingPresignedURL(S3PresignedURL, m_strFileToUpload, fileTransferProgress);
+
+
+                    ShowTotalProcess = false;
+                });
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+
+
 
         public object CompressUserControl
         {
